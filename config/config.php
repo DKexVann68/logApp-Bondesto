0990f490a02bddcf85ab40da5330249074a8df2d
KExVann <90809295+DKexVann68@users.noreply.github.com>
2021-10-28 19:36:26 +0800

    Done Practice 5-06

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+	define('ROOT_URL', 'http://localhost/logApp-Bondesto/');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6447345');
+	define('DB_PASS', 'L5TZhiepuJ');
+	define('DB_NAME', 'sql6447345');
+
+
+?>
